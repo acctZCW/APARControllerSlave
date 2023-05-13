4e7054635b3c49c673bbcefb21c6c00cea67a36c
acctZCW <3120210682@bit.edu.cn>
2023-05-13 16:42:08 +0800

    20230513 16:41 add pe44820/43703 spi

--- Core/Src/bsp_pe43703.c
+++ Core/Src/bsp_pe43703.c
@@ -44,3 +44,42 @@ int writePE43703(PE43703TypeDef unit, uint8_t data){
 
     return 1;
 }
+
+int writePE43703WithSpi(PE43703TypeDef unit, uint8_t data, uint8_t addr){
+    data = data&0x7F;
+
+    HAL_GPIO_WritePin(unit.clkPort,unit.clkPin,GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(unit.lePort,unit.lePin,GPIO_PIN_RESET);
+
+    uint8_t w_data;
+    // write data into unit.
+    for(int i = 0; i < 7; i++){
+        w_data = (data >> i) & 1;
+        if(w_data == 1)
+            HAL_GPIO_WritePin(unit.siPort,unit.siPin,GPIO_PIN_SET);
+        else
+            HAL_GPIO_WritePin(unit.siPort,unit.siPin,GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(unit.clkPort,unit.clkPin,GPIO_PIN_SET);
+        HAL_Delay_Us(5);
+        HAL_GPIO_WritePin(unit.clkPort,unit.clkPin,GPIO_PIN_RESET);
+    }
+
+    // write addr
+    for(int i = 0; i< 3; i++){
+        w_data = (addr >> i) & 1;
+        if(w_data == 1)
+            HAL_GPIO_WritePin(unit.siPort,unit.siPin,GPIO_PIN_SET);
+        else
+            HAL_GPIO_WritePin(unit.siPort,unit.siPin,GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(unit.clkPort,unit.clkPin,GPIO_PIN_SET);
+        HAL_Delay_Us(5);
+        HAL_GPIO_WritePin(unit.clkPort,unit.clkPin,GPIO_PIN_RESET);
+    }
+
+    // turn off
+    HAL_GPIO_WritePin(unit.lePort,unit.lePin,GPIO_PIN_SET);
+    HAL_Delay_Us(10);
+    HAL_GPIO_WritePin(unit.lePort,unit.lePin,GPIO_PIN_RESET);
+
+    return 1;
+}
--- Core/Src/bsp_pe44820.c
+++ Core/Src/bsp_pe44820.c
@@ -88,3 +88,41 @@ int writePE44820(PE44820TypeDef unit, uint8_t data){
 
     return 1;
 }
+
+int writePE44820WithSPi(PE44820TypeDef unit, uint8_t data, uint8_t addr){
+    data = data & 0xFF;
+
+    HAL_GPIO_WritePin(unit.clkPort,unit.clkPin,GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(unit.lePort,unit.lePin,GPIO_PIN_RESET);
+    uint8_t w_data;
+    // write data into unit.
+    for(int i = 0; i < 8; i++){
+        w_data = (data >> i) & 1;
+        if(w_data == 1)
+            HAL_GPIO_WritePin(unit.siPort,unit.siPin,GPIO_PIN_SET);
+        else
+            HAL_GPIO_WritePin(unit.siPort,unit.siPin,GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(unit.clkPort,unit.clkPin,GPIO_PIN_SET);
+        HAL_Delay_Us(5);
+        HAL_GPIO_WritePin(unit.clkPort,unit.clkPin,GPIO_PIN_RESET);
+    }
+
+    // write addr
+    for(int i = 0; i < 3; i++){
+        w_data = (addr >> i) & 1;
+        if(w_data == 1)
+            HAL_GPIO_WritePin(unit.siPort,unit.siPin,GPIO_PIN_SET);
+        else
+            HAL_GPIO_WritePin(unit.siPort,unit.siPin,GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(unit.clkPort,unit.clkPin,GPIO_PIN_SET);
+        HAL_Delay_Us(5);
+        HAL_GPIO_WritePin(unit.clkPort,unit.clkPin,GPIO_PIN_RESET);
+    }
+
+    // turn off
+    HAL_GPIO_WritePin(unit.lePort,unit.lePin,GPIO_PIN_SET);
+    HAL_Delay_Us(10);
+    HAL_GPIO_WritePin(unit.lePort,unit.lePin,GPIO_PIN_RESET);
+
+    return 1;
+}
